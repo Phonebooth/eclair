783ae760a55a630610f6c1594d61a12c92ef49a8
Jesse Stimpson <jstimpson@bandwidth.com>
2015-11-06 17:14:12 -0500

    config merging

--- eclair.erl
+++ eclair.erl
@@ -23,8 +23,8 @@ main(["bootstrap"|Args]) ->
     bootstrap(Args);
 main(Args) ->
     {Props, _Data} = args_to_properties(Args),
-    Props2 = merge_doteclair_config(Props),
-    Props3 = merge_hardcoded_config(Props2),
+    Props2 = ensure_with_doteclair_config(Props),
+    Props3 = ensure_with_hardcoded_config(Props2),
     ok = application:start(ibrowse),
     HostData = gather_host_data(),
     Path = build_path(proplists:get_value(root, Props3), HostData),
@@ -68,7 +68,7 @@ place_file(Obj, RelativePath) ->
         ok ->
             io:format("Placed new file ~s successfully~n", [RelativePath]);
         {error, eexist} ->
-            io:format("File collision detected for ~s~n", [RelativePath]),
+            io:format("Merging in new data for ~s~n", [RelativePath]),
             handle_file_collision(RelativePath, Bytes)
     end.
 
@@ -76,8 +76,16 @@ handle_file_collision(RelativePath, Bytes) ->
     % Merge proplists in Bytes with proplists at RelativePath
     {ok, OldProplists} = file:consult(RelativePath),
     {ok, NewProplists} = bytes_consult(Bytes),
-    % TODO - merge proplists by building all xpaths first
-    ok.
+    MergedProplists = lists:map(fun(X) ->
+                lists:foldl(fun(Y, A) -> merge(Y, A, use_source) end,
+                    X, NewProplists)
+        end,
+                OldProplists),
+    {ok, Io} = file:open(RelativePath, [write]),
+    lists:foreach(fun(X) ->
+                file:write(Io, io_lib:format("~p.~n", [X]))
+        end, MergedProplists),
+    file:close(Io).
 
 bytes_consult(Bytes) ->
     % I could not find a way to consult bytes, so we use a temp file
@@ -166,7 +174,7 @@ args_to_properties([Value|Args], {value, Key}, Proplist, Data) ->
 bootstrap(Args) ->
     file:make_dir(".eclair"),
     {Props, _Data} = args_to_properties(Args),
-    Props2 = merge_doteclair_config(Props),
+    Props2 = ensure_with_doteclair_config(Props),
     AccessKey = getline_arg(access_key, Props2),
     SecretKey = getline_arg(secret_key, Props2),
     Root = getline_arg(root, Props2),
@@ -193,20 +201,20 @@ getline_arg(Key, Props) ->
             V1
     end.
 
-merge_doteclair_config(Props) ->
+ensure_with_doteclair_config(Props) ->
     case file:consult(".eclair/secure.config") of
         {error, enoent} ->
             Props;
         {ok, [Access]} ->
-            merge_proplist(Access, Props)
+            ensure_proplist(Access, Props)
     end.
 
-merge_hardcoded_config(Props) ->
-    merge_proplist([{access_key, ?AccessKey},
+ensure_with_hardcoded_config(Props) ->
+    ensure_proplist([{access_key, ?AccessKey},
                     {secret_key, ?SecretKey},
                     {root, ?Root}], Props).
 
-merge_proplist(From, To) ->
+ensure_proplist(From, To) ->
     Props2 = lists:foldl(fun({X, Y}, A) ->
                 case lists:keyfind(X, 1, A) of
                     false ->
@@ -220,3 +228,135 @@ merge_proplist(From, To) ->
 write_doteclair(Props) ->
     Bytes = io_lib:format("~p.~n", [Props]),
     file:write_file(".eclair/secure.config", Bytes).
+
+merge(Src, Dest, MismatchPolicy) ->
+    DestPaths = xpaths(Dest),
+    SrcPaths = xpaths(Src),
+    DestKeyset = sets:from_list(element(1, lists:unzip(DestPaths))),
+    SrcKeyset = sets:from_list(element(1, lists:unzip(SrcPaths))),
+    AddsKeyset = sets:subtract(SrcKeyset, DestKeyset),
+    UpdatesKeyset = sets:intersection(SrcKeyset, DestKeyset),
+
+    % Find xpaths in the source that would clobber depth in the dest.
+    % We will either (A) ignore these paths such that depth is always preferred
+    % or (B) delete any paths in the dest that begin with the shallow paths,
+    % thus prefering the source data.
+    AllKeyset = sets:union(SrcKeyset, DestKeyset),
+    AllKeylist = sets:to_list(AllKeyset),
+    Stems = [ lists:sublist(X, 1, length(X)-1) || X <- AllKeylist ],
+    Leaves = [ lists:last(X) || X <- AllKeylist ],
+    ShallowLeaves = lists:filter(fun(X) ->
+                lists:any(fun(Z) -> Z end, [ lists:member(X, Y) || Y <- Stems ])
+        end, Leaves),
+    ShallowPaths = lists:filter(fun(X) ->
+                lists:member(lists:last(X), ShallowLeaves)
+        end, AllKeylist),
+
+    {AddsKeyset2, UpdatesKeyset2, DestPaths2} = 
+        case MismatchPolicy of
+            favor_depth ->
+                IgnoreSrcKeyset = sets:from_list(ShallowPaths),
+                AK2 = sets:subtract(AddsKeyset, IgnoreSrcKeyset),
+                UK2 = sets:subtract(UpdatesKeyset, IgnoreSrcKeyset),
+                {AK2, UK2, DestPaths};
+            use_source ->
+                DP2 = lists:filter(fun({X, _}) ->
+                            not lists:any(fun(Y) ->
+                                        lists:sublist(X, 1, length(Y)) =:= Y
+                                end, ShallowPaths)
+                    end, DestPaths),
+                {AddsKeyset, UpdatesKeyset, DP2}
+        end,
+
+    AddsKeylist = sets:to_list(AddsKeyset2),
+    UpdatesKeylist = sets:to_list(UpdatesKeyset2),
+
+    AddsPaths = [ {X, proplists:get_value(X, SrcPaths)} || X <- AddsKeylist ],
+    UpdatesPaths = [ {X, proplists:get_value(X, SrcPaths)} || X <- UpdatesKeylist ],
+
+    Merged = lists:sort(
+        lists:foldl(fun(U = {KU, _VU}, Acc) ->
+                proplists:delete(KU, Acc) ++ [U]
+        end, DestPaths2, UpdatesPaths) ++ AddsPaths
+    ),
+    from_xpaths(Merged).
+
+xpaths(A) ->
+    RevResult = depthfold(fun(Keys, Val, Accum) ->
+                [{Keys, Val}|Accum]
+        end, [], A),
+    lists:reverse(RevResult).
+
+depthfold(Fun, Accum, A) ->
+    {_, Result} = depthfold_(Fun, A, {[], Accum}),
+    Result.
+
+depthfold_(_Fun, [], {Keys, Accum}) ->
+    {Keys, Accum};
+depthfold_(Fun, [{Key, Value}|A], {Keys, Accum}) ->
+    {_Keys2, Accum2} = depthfold_(Fun, Value, {[Key|Keys], Accum}),
+    depthfold_(Fun, A, {Keys, Accum2});
+depthfold_(Fun, Value, {Keys, Accum}) ->
+    {Keys, Fun(lists:reverse(Keys), Value, Accum)}.
+
+from_xpaths(XPaths) ->
+    lists:foldl(fun ensure_proplist_keypath/2, [], XPaths).
+
+ensure_proplist_keypath({[Key|KeyPath], Value}, Proplist) ->
+    case proplists:lookup(Key, Proplist) of
+        none ->
+            case KeyPath of
+                [] ->
+                    lists:sort([{Key, Value}|Proplist]);
+                _ ->
+                    Sub = ensure_proplist_keypath({KeyPath, Value}, []),
+                    lists:sort([{Key, Sub}|Proplist])
+            end;
+        {_, Sub} ->
+            Sub2 = ensure_proplist_keypath({KeyPath, Value}, Sub),
+            lists:sort([{Key, Sub2}|proplists:delete(Key, Proplist)])
+    end.
+
+%test_merge() ->
+%    Src = [{a, [
+%                {aa, "srcaa"}
+%            ]},
+%           {b,
+%               [
+%                   {ba,
+%                       [
+%                           {baa, "srcbaa"}
+%                       ]
+%                   }
+%               ]
+%           },
+%           {c, "c"}],
+%      Dest = [{a, 
+%                [
+%                    {aa, [
+%                            {aaa, "aaa"}
+%                        ]}, 
+%                    {ab,"ab"}
+%                ]
+%            },
+%          {b,
+%              [
+%                  {ba, [
+%                          {baa, "baa"},
+%                          {bab, "bab"}
+%                      ]
+%                  },
+%                  {bb, "bb"},
+%                  {bc, [
+%                          {bca, [
+%                                  {bcaa, "bcaa"}
+%                              ]
+%                          }
+%                      ]
+%                  }
+%              ]
+%          },
+%          {d, 1}
+%        ],
+%        [{use_source, merge(Src, Dest, use_source)},
+%            {favor_depth, merge(Src, Dest, favor_depth)}].
